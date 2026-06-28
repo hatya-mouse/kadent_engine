556bc4bef5cbeb3ec1d13dab819114cef16c7171
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-06-28 17:03:01 +0900

    fix: create new MIDI buffer every time a new stream if built

--- src/thread/audio_thread.rs
+++ src/thread/audio_thread.rs
@@ -7,7 +7,7 @@ use crate::{
     },
 };
 use cpal::traits::{HostTrait, StreamTrait};
-use ringbuf::traits::{Producer, Split};
+use ringbuf::traits::{Consumer, Producer, Split};
 use std::sync::{
     Arc, Mutex,
     atomic::{AtomicBool, AtomicUsize, Ordering},
@@ -17,13 +17,14 @@ use std::sync::{
 pub(super) fn audio_thread(
     command_rx: mpsc::Receiver<AudioCommand>,
     result_tx: mpsc::Sender<Result<AudioResult, AudioError>>,
-    midi_cons: ringbuf::HeapCons<MidiEvent>,
+    mut midi_cons: ringbuf::HeapCons<MidiEvent>,
     vu_prod: ringbuf::HeapProd<f32>,
     playhead: Arc<AtomicUsize>,
     audio_ctx: AudioContext,
     initial_project: Project,
 ) {
     let (mut command_prod, command_cons) = ringbuf::HeapRb::<AudioCommand>::new(64).split();
+    let (mut midi_sub_prod, midi_sub_cons) = ringbuf::HeapRb::<MidiEvent>::new(64).split();
 
     // Create a mixer with the given initial project
     let pending_project = Arc::new(Mutex::new(None));
@@ -52,7 +53,7 @@ pub(super) fn audio_thread(
     let callback_ctx = Arc::new(Mutex::new(OutputCallbackContext {
         mixer,
         command_cons,
-        midi_cons,
+        midi_cons: midi_sub_cons,
         vu_prod,
         pending_project: pending_arc,
     }));
@@ -76,65 +77,77 @@ pub(super) fn audio_thread(
     }
 
     // Create a message loop
-    for command in command_rx {
-        match command {
-            AudioCommand::Play => {
-                is_playing.store(true, Ordering::Release);
-            }
-            AudioCommand::Pause => {
-                is_playing.store(false, Ordering::Release);
-            }
-            AudioCommand::UpdateProject(mut new_project) => {
-                // Increment the current generation by one to mark it as the latest
-                let current_gen = generation.fetch_add(1, Ordering::SeqCst) + 1;
-                let gen_arc = Arc::clone(&generation);
-                let pending_arc = Arc::clone(&pending_project);
-                let result_tx = result_tx.clone();
-                std::thread::spawn(move || {
-                    // Prepare the project before applying the project
-                    if let Err(err) = new_project.prepare() {
-                        result_tx.send(Err(AudioError::GraphError(err))).unwrap();
-                        return;
-                    }
-
-                    // Check if the project is the latest one
-                    if gen_arc.load(Ordering::SeqCst) == current_gen {
-                        // Send the new project to the audio playback thread
-                        *pending_arc.lock().unwrap() = Some(*new_project);
-                    }
-                });
-            }
-            AudioCommand::ExportAudio(project) => {
-                let result_tx = result_tx.clone();
-                export::spawn_export_thread(result_tx, *project);
-            }
-            AudioCommand::SetOutputDevice(device) => {
-                stream.take();
-
-                stream = Some(output_callback(
-                    callback_ctx.clone(),
-                    device,
-                    config,
-                    callback_state.clone(),
-                ));
-
-                if let Some(stream) = stream.as_ref()
-                    && let Err(err) = stream.play()
-                {
-                    result_tx
-                        .send(Err(AudioError::PlayStreamError(err)))
-                        .unwrap();
+    loop {
+        while let Ok(command) = command_rx.try_recv() {
+            match command {
+                AudioCommand::Play => {
+                    is_playing.store(true, Ordering::Release);
                 }
-            }
-            AudioCommand::Seek(_) | AudioCommand::ArmTrack(_) | AudioCommand::DisarmTrack => {
-                if let Err(command) = command_prod.try_push(command) {
-                    result_tx
-                        .send(Err(AudioError::CommandFailed(command)))
-                        .unwrap();
+                AudioCommand::Pause => {
+                    is_playing.store(false, Ordering::Release);
+                }
+                AudioCommand::UpdateProject(mut new_project) => {
+                    // Increment the current generation by one to mark it as the latest
+                    let current_gen = generation.fetch_add(1, Ordering::SeqCst) + 1;
+                    let gen_arc = Arc::clone(&generation);
+                    let pending_arc = Arc::clone(&pending_project);
+                    let result_tx = result_tx.clone();
+                    std::thread::spawn(move || {
+                        // Prepare the project before applying the project
+                        if let Err(err) = new_project.prepare() {
+                            result_tx.send(Err(AudioError::GraphError(err))).unwrap();
+                            return;
+                        }
+
+                        // Check if the project is the latest one
+                        if gen_arc.load(Ordering::SeqCst) == current_gen {
+                            // Send the new project to the audio playback thread
+                            *pending_arc.lock().unwrap() = Some(*new_project);
+                        }
+                    });
+                }
+                AudioCommand::ExportAudio(project) => {
+                    let result_tx = result_tx.clone();
+                    export::spawn_export_thread(result_tx, *project);
+                }
+                AudioCommand::SetOutputDevice(device) => {
+                    stream.take();
+
+                    // Create a new MIDI ring buffer and split it into producer and consumer
+                    let (new_sub_prod, new_sub_cons) =
+                        ringbuf::HeapRb::<MidiEvent>::new(64).split();
+                    midi_sub_prod = new_sub_prod;
+
+                    callback_ctx.lock().unwrap().midi_cons = new_sub_cons;
+
+                    stream = Some(output_callback(
+                        callback_ctx.clone(),
+                        device,
+                        config,
+                        callback_state.clone(),
+                    ));
+
+                    if let Some(stream) = stream.as_ref()
+                        && let Err(err) = stream.play()
+                    {
+                        result_tx
+                            .send(Err(AudioError::PlayStreamError(err)))
+                            .unwrap();
+                    }
+                }
+                AudioCommand::Seek(_) | AudioCommand::ArmTrack(_) | AudioCommand::DisarmTrack => {
+                    if let Err(command) = command_prod.try_push(command) {
+                        result_tx
+                            .send(Err(AudioError::CommandFailed(command)))
+                            .unwrap();
+                    }
                 }
             }
         }
-    }
 
-    stream.take();
+        // Send the MIDI events from the midi_cons to the midi_sub_prod
+        while let Some(midi_event) = midi_cons.try_pop() {
+            midi_sub_prod.try_push(midi_event).ok();
+        }
+    }
 }
