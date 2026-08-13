5124a9b67f70214d8b0d88bdab5dd4c279e31e06
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-13 18:53:31 +0900

    fix: adjust edge case handling in AUdioRegion::render_buffer

--- src/track/audio_track/audio_region.rs
+++ src/track/audio_track/audio_region.rs
@@ -20,14 +20,20 @@ pub struct AudioRegion {
 
     /// Cached factor used to convert relative global ticks to local sample indices in the audio data.
     ticks_to_local_factor: f64,
+    /// Cached region start global sample index in the current playback context.
+    region_start_samples: usize,
+    /// Cached region end global sample index in the current playback context.
+    region_end_samples: usize,
 }
 
 impl AudioRegion {
     // --- REGION PROCESSING ---
 
-    pub(super) fn prepare(&mut self, audio_ctx: &AudioContext) {
+    pub(super) fn prepare(&mut self, tempo_map: &TempoMap, audio_ctx: &AudioContext) {
         self.ticks_to_local_factor =
-            (60f64 * self.info.sample_rate as f64) / (audio_ctx.resolution as f64 * self.info.bpm);
+            (60.0 * self.info.sample_rate as f64) / (audio_ctx.resolution as f64 * self.info.bpm);
+        self.region_start_samples = tempo_map.ticks_to_samples(self.start);
+        self.region_end_samples = tempo_map.ticks_to_samples(self.end());
     }
 
     /// Reads and writes the audio data to the given buffer based on the current playhead position and the buffer size.
@@ -44,9 +50,27 @@ impl AudioRegion {
         tempo_map: &TempoMap,
         playback_ctx: &PlaybackContext,
     ) {
+        // Skip processing if the buffer falls entirely outside the region's range
+        let buffer_end = playhead + playback_ctx.buffer_size;
+        if buffer_end <= self.region_start_samples || playhead >= self.region_end_samples {
+            return;
+        }
+
+        // Calculate where to start and end reading from the audio data
+        // This is to handle edge cases
+        // Buffer: |<-- Current buffer block -->|
+        //         ^ playhead                   ^ buffer_end
+        // Region:      [<-- Region -->]
+        // global_start ^              ^ global_end
+        let global_start = playhead.max(self.region_start_samples);
+        let global_end = buffer_end.min(self.region_end_samples);
+
         // Convert the playhead position to a sample index in the audio data
-        let local_start = self.calculate_local_samples(playhead, tempo_map);
-        let local_end = self.calculate_local_samples(playhead + buffer.len(), tempo_map);
+        let local_start = self.calculate_local_samples(global_start, tempo_map);
+        let local_end = self.calculate_local_samples(global_end, tempo_map);
+        if local_start >= local_end {
+            return;
+        }
 
         // Get the slice of the audio data from the audio source
         let Some(data) = self.data_source.get_data(local_start..local_end) else {
@@ -64,9 +88,17 @@ impl AudioRegion {
             tempo_map,
         );
 
-        // Interleave and add the resampled data to the buffer
-        // The buffer must have MAX_CHANNELS channels
-        add_samples_interleaved(&resampled, buffer, self.info.channels, MAX_CHANNELS);
+        // Calculate the output buffer offset where writing should start
+        let buffer_offset = self.region_start_samples.saturating_sub(playhead) * MAX_CHANNELS;
+        // Interleave and add the resampled data to the buffer; The buffer must have MAX_CHANNELS channels
+        if buffer_offset < buffer.len() {
+            add_samples_interleaved(
+                &resampled,
+                &mut buffer[buffer_offset..],
+                self.info.channels,
+                MAX_CHANNELS,
+            );
+        }
     }
 
     // --- CALCULATIONS ---
@@ -81,7 +113,7 @@ impl AudioRegion {
     #[inline]
     fn calculate_local_samples(&self, global_samples: usize, tempo_map: &TempoMap) -> usize {
         let global_ticks = tempo_map.samples_to_ticks(global_samples);
-        let delta_ticks = (global_ticks.0 - self.start.0) as f64;
+        let delta_ticks = global_ticks.0.saturating_sub(self.start.0) as f64;
         (delta_ticks * self.ticks_to_local_factor) as usize
     }
 }
