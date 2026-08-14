8a45fd8b6477a2015a09e77024b52d16f7bb1aa7
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-14 21:48:58 +0900

    feat: round src_pos

--- src/track/audio_track/resampler.rs
+++ src/track/audio_track/resampler.rs
@@ -21,7 +21,7 @@ pub fn resample_channels(
         .enumerate()
         .for_each(|(sample, sample_buffer)| {
             // Calculate the corresponding position in the source array
-            let src_pos = (sample as f64 * inv_ratio).ceil();
+            let src_pos = (sample as f64 * inv_ratio).round();
             let index = src_pos as usize;
 
             if index + 1 < src_samples {
