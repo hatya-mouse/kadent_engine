f0efcfa01480e4e355386c7e05546b526f812d8f
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-08-15 22:02:28 +0900

    feat: make seconds_to_samples, samples_to_seconds and convert_rate_with_ratio public

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,12 @@
+/// Converts seconds to samples with the given sample rate.
 #[inline]
-pub(crate) fn seconds_to_samples(seconds: f64, sample_rate: u64) -> usize {
+pub fn seconds_to_samples(seconds: f64, sample_rate: u64) -> usize {
     (seconds * sample_rate as f64).round() as usize
 }
 
+/// Converts samples to seconds with the given sample rate.
 #[inline]
-pub(crate) fn samples_to_seconds(samples: usize, sample_rate: u64) -> f64 {
+pub fn samples_to_seconds(samples: usize, sample_rate: u64) -> f64 {
     samples as f64 / sample_rate as f64
 }
 
@@ -15,6 +17,6 @@ pub(crate) fn samples_to_seconds(samples: usize, sample_rate: u64) -> f64 {
 /// ratio = output_sample_rate / input_sample_rate
 /// ```
 #[inline]
-pub(crate) fn convert_rate_with_ratio(samples: usize, ratio: f64) -> usize {
+pub fn convert_rate_with_ratio(samples: usize, ratio: f64) -> usize {
     (samples as f64 * ratio).round() as usize
 }
